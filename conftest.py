867aee29dced49cadbe71e88e8bb54a80229c53a
nathanroberts55 <nathan.roberts.15@cnu.edu>
2023-08-09 13:28:30 -0400

    Trying again

--- conftest.py
+++ conftest.py
@@ -2,10 +2,13 @@ import pytest
 import requests
 import subprocess
 import time
+import os
 
 
 def server():
     # Start the server as a separate process and redirect its output to a file
+    python_path: str = None
+
     with open("server.log", "w") as f:
         subprocess.Popen(["python", "main.py"], stdout=f, stderr=subprocess.STDOUT)
 
@@ -18,7 +21,7 @@ def start_server():
     time.sleep(5)
     # Check if the server is running
     try:
-        response = requests.get("http://localhost:8000")
+        response = requests.get("http://localhost:8080")
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         pytest.exit(f"Failed to start server: {e}")
