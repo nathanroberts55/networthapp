ef0520e36eb8bb65d717e3c708727231878261ae
nathanroberts55 <nathan.roberts.15@cnu.edu>
2023-08-10 19:56:11 -0400

    reorganized the order of code
    added visualization

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
+import plotly.graph_objects as go
 from nicegui import ui
 from database import db
 
+# ============== Configuration =======================
 ui.dark_mode().enable()
 
 # Initialize the SQLite Database
@@ -10,36 +12,23 @@ if not os.path.exists("networth.db"):
     print("Database does not exist... Creating Database")
     db.initialize_database()
 
+# =============== Global Variables ====================
 select_data = []
 my_data = db.get_all_items()
-line_df = db.get_dataframe()
-
-# App Title
-ui.label("Net Worth Calculator").classes("text-4xl")
-ui.splitter(horizontal=True)
-
-# Data Entry and Table
-ui.label("Line Items").classes("text-2xl")
-my_table: ui.aggrid = ui.aggrid(
-    {
-        "defaultColDef": {"flex": 1},
-        "columnDefs": [
-            {"headerName": "Name", "field": "name"},
-            {"headerName": "Type", "field": "type"},
-            {"headerName": "Status", "field": "status"},
-            {
-                "headerName": "Amount",
-                "field": "amount",
-                "valueFormatter": lambda params: f"${float(params.value):.2f}",
-            },
-            {"headerName": "ID", "field": "id", "hide": True},
-        ],
-        "rowData": sorted(db.get_all_items(), key=lambda data: data["name"]),
-        "rowSelection": "multiple",
-    }
-).classes("m-auto")
+colors = [
+    "#00CBFF",
+    "#354A53",
+    "#98AFBA",
+    "#F0AE71",
+    "#B5793F",
+    "#FCFCD4",
+    "#964A00",
+    "#594317",
+    "#8D7345",
+]
 
 
+# =============== API Handlers ======================
 def add_new_data() -> None:
     """
     Adds a new line item to the database and updates the table with the new data.
@@ -66,11 +55,16 @@ def add_new_data() -> None:
 
     # Close Dialog and Reset Values (inputs will reset themselves)
     new_data_dialog.close()
+    add_name.set_value(None)
+    add_type.set_value(None)
     add_status.set_value(None)
     add_amount.set_value(None)
 
     # Update Table
     my_table.update()
+
+    net_breakdown_cards.refresh()
+    net_composition_plot.refresh()
 
 
 def update_data() -> None:
@@ -100,34 +94,11 @@ def update_data() -> None:
     edit_data_dialog.close()
     my_table.update()
 
-
-# Dialog for Adding Input
-with ui.dialog() as new_data_dialog:
-    with ui.card():
-        add_name = ui.input(label="Add Name")
-        add_type = ui.input(label="Add Type")
-        add_status = ui.select(
-            label="Add Status", options=["Asset", "Liability"], value=None
-        ).classes("w-full")
-        add_amount = ui.number(label="Add Amount", format="%.2f", value=None).on(
-            "blur", lambda: add_amount.update()
-        )
-        ui.button("Save New Stream", on_click=add_new_data)
-
-# Dialog for Editing Input
-with ui.dialog() as edit_data_dialog:
-    with ui.card():
-        edit_name = ui.input(label="Edit Name")
-        edit_type = ui.input(label="Edit Type")
-        edit_status = ui.select(
-            label="Edit Status", options=["Asset", "Liability"]
-        ).classes("w-full")
-        edit_amount = ui.number(label="Edit Amount", format="%.2f").on(
-            "blur", lambda: edit_amount.update()
-        )
-        ui.button("Edit Stream", on_click=update_data)
+    net_breakdown_cards.refresh()
+    net_composition_plot.refresh()
 
 
+# ============== Event Handlers ===================
 def opendata(e) -> None:
     # Open Dialog to add new data to table
     new_data_dialog.open()
@@ -162,6 +133,9 @@ async def removedata() -> None:
 
     my_table.update()
 
+    net_breakdown_cards.refresh()
+    net_composition_plot.refresh()
+
 
 async def editdata() -> None:
     """
@@ -187,6 +161,106 @@ async def editdata() -> None:
     edit_data_dialog.open()
 
 
+# ============== UI Functions =====================
+@ui.refreshable
+def net_composition_plot() -> None:
+    line_df = db.get_dataframe()
+    fig = go.Figure(
+        data=[
+            go.Pie(
+                labels=line_df["name"],
+                values=line_df["amount"],
+                hole=0.5,
+                title="Net Worth Composition",
+                titleposition="top center",
+                marker=dict(colors=colors),
+            )
+        ]
+    )
+
+    # Set the background color to black
+    fig.update_layout(
+        plot_bgcolor="#121212", paper_bgcolor="#121212", font={"color": "white"}
+    )
+
+    # Set the text color to white
+    fig.update_traces(textfont=dict(color="white"))
+
+    ui.plotly(fig).classes("w-7/8 m-auto")
+
+
+@ui.refreshable
+def net_breakdown_cards() -> None:
+    line_df = db.get_dataframe()
+    sum_liabilties = line_df.loc[line_df["status"] == "Liability", "amount"].sum()
+    sum_assets = line_df.loc[line_df["status"] == "Asset", "amount"].sum()
+    net_worth = sum_assets - sum_liabilties
+
+    with ui.row().classes("w-full justify-evenly") as tile_row:
+        with ui.card().classes("w-1/4 place-content-center") as total_items:
+            ui.label("Net Worth").classes("text-xl").classes("m-auto")
+            ui.label(f"${float(net_worth):,.2f}").classes("text-center").classes(
+                "m-auto"
+            )
+        with ui.card().classes("w-1/4 place-content-center") as total_assets:
+            ui.label("Total Assets").classes("text-xl").classes("m-auto")
+            ui.label(f"${float(sum_assets):,.2f}").classes("m-auto")
+        with ui.card().classes("w-1/4 place-content-center") as total_liabilities:
+            ui.label("Total Liabilities").classes("text-xl").classes("m-auto")
+            ui.label(f"${float(sum_liabilties):,.2f}").classes("m-auto")
+
+
+# =============== Main UI ======================
+
+# App Title
+ui.label("Net Worth Calculator").classes("text-4xl")
+ui.splitter(horizontal=True)
+
+# ------ Data Entry and Table ------------
+ui.label("Line Items").classes("text-2xl")
+my_table: ui.aggrid = ui.aggrid(
+    {
+        "defaultColDef": {"flex": 1},
+        "columnDefs": [
+            {"headerName": "Name", "field": "name"},
+            {"headerName": "Type", "field": "type"},
+            {"headerName": "Status", "field": "status"},
+            {"headerName": "Amount", "field": "amount"},
+            {"headerName": "ID", "field": "id", "hide": True},
+        ],
+        "rowData": sorted(db.get_all_items(), key=lambda data: data["name"]),
+        "rowSelection": "multiple",
+    }
+).classes("m-auto")
+
+
+# Dialog for Adding Input
+with ui.dialog() as new_data_dialog:
+    with ui.card():
+        add_name = ui.input(label="Add Name")
+        add_type = ui.input(label="Add Type")
+        add_status = ui.select(
+            label="Add Status", options=["Asset", "Liability"], value=None
+        ).classes("w-full")
+        add_amount = ui.number(label="Add Amount", format="%.2f", value=None).on(
+            "blur", lambda: add_amount.update()
+        )
+        ui.button("Save New Stream", on_click=add_new_data)
+
+# Dialog for Editing Input
+with ui.dialog() as edit_data_dialog:
+    with ui.card():
+        edit_name = ui.input(label="Edit Name")
+        edit_type = ui.input(label="Edit Type")
+        edit_status = ui.select(
+            label="Edit Status", options=["Asset", "Liability"]
+        ).classes("w-full")
+        edit_amount = ui.number(label="Edit Amount", format="%.2f").on(
+            "blur", lambda: edit_amount.update()
+        )
+        ui.button("Edit Stream", on_click=update_data)
+
+
 # CRUD Buttons
 with ui.row() as button_row:
     ui.button("Add", color="green", on_click=lambda e: opendata(e))
@@ -195,22 +269,10 @@ with ui.row() as button_row:
 
 ui.splitter(horizontal=True)
 
-# Reporting
+# ----------- Reporting -------------
+
 ui.label("Net Worth Breakdown").classes("text-2xl")
-
-sum_liabilties = line_df.loc[line_df["status"] == "Liability", "amount"].sum()
-sum_assets = line_df.loc[line_df["status"] == "Asset", "amount"].sum()
-net_worth = sum_assets - sum_liabilties
-
-with ui.row().classes("w-full justify-evenly") as tile_row:
-    with ui.card().classes("w-1/4 place-content-center") as total_items:
-        ui.label("Net Worth").classes("text-xl").classes("m-auto")
-        ui.label(f"${float(net_worth):,.2f}").classes("text-center").classes("m-auto")
-    with ui.card().classes("w-1/4 place-content-center") as total_assets:
-        ui.label("Total Assets").classes("text-xl").classes("m-auto")
-        ui.label(f"${float(sum_assets):,.2f}").classes("m-auto")
-    with ui.card().classes("w-1/4 place-content-center") as total_liabilities:
-        ui.label("Total Liabilities").classes("text-xl").classes("m-auto")
-        ui.label(f"${float(sum_liabilties):,.2f}").classes("m-auto")
+net_breakdown_cards()
+net_composition_plot()
 
 ui.run(title="Net Worth Tracker", favicon="assets\\asset.png")
